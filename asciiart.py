9e6d5b2c33467f2908d525f6030e20fa556922e0
LamkQmobile <lamkqmobile2021@gmail.com>
2021-02-08 11:02:41 +0700

    Added some functionality
    - Confirmation
    - Arguments via command line

--- asciiart.py
+++ asciiart.py
@@ -14,9 +14,11 @@ mode: sequence or random (0 for sequence; 1 for random)
 
 """
 
+#count for sequence mode
 count = 0
 
 def argvinput():
+	#count if the number of parameter is correct
     arg = sys.argv
     if len(arg) != 7:
         print('Wrong number of parameters')
@@ -26,24 +28,43 @@ def argvinput():
 def selectchar(charset, mode):
     global count
     if len(charset) == 1:
+    	#if the word is only 1 then you know
         return charset
     else:
         if mode == 1:
+        	#random mode
             return random.choice(charset)
         else:
+        	#sequence mode
             count = count % len(charset)
             count += 1
             return charset[count - 1]
 
 
 def main():
+
+	#read input
     arg = argvinput()
+
+    #open the image according to the input
     img = Image.open(arg[1])
+
+    #image scale factor
     scale = float(arg[3])
+
+    #font wide and high
     W, H = 10, 18
+
+    #image size
     w,h = img.size
+
+    #resize image
     img = img.resize((int(scale*w),int(scale*h*(W/H))),Image.NEAREST)
+
+    #new image size
     w,h = img.size
+
+    #get the pixels color set
     pixels = img.load()
 
     ouputimg = Image.new('RGB', (W*w,H*h),color=(0,0,0))
@@ -51,7 +72,18 @@ def main():
 
     font = ImageFont.truetype('C:\\Windows\\Fonts\\micross.ttf', int(arg[4]))
 
-    print(w,h)
+    print(f'')
+
+    cap = {0: 'Sequence', 1: 'Random'}
+
+    print(f'Input file: {arg[1]}')
+    print(f'Output file: {arg[2]}')
+    print(f'Scale Factor: {arg[3]}')
+    print(f'Font size: {arg[4]}')
+    print(f'Word: {arg[5]}')
+    print(f'Mode: {cap[int(arg[6])]}')
+    
+    print(f'Loop: {w} x {h}')
 
     input('Procceed?')
 
