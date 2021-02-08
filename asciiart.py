8151f1f9b6f8121e939c49b107fbe2197443dcb4
LamkQmobile <lamkqmobile2021@gmail.com>
2021-02-08 08:51:58 +0700

    Still Undone
    F**k

---
+++ asciiart.py
@@ -0,0 +1,15 @@
+import sys
+import numpy as np
+from PIL import Image, ImageDraw, ImageFont
+import math
+
+"""
+Program format
+python file.py infile.png outfile.png scalefactor word mode
+
+scalefactor: to scale the image bigger or smaller
+word: the character set
+mode: sequence or random (0 for sequence; 1 for random)
+
+"""
+
--- convert.py
+++
@@ -1,5 +0,0 @@
-import sys
-import numpy as np
-from PIL import Image, ImageDraw, ImageFont
-import math
-
