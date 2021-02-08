b2233683125f38a9ce0c433c3f1b6abb1c9ef412
LamkQmobile <lamkqmobile2021@gmail.com>
2021-02-08 09:46:10 +0700

    Update asciiart.py

--- asciiart.py
+++ asciiart.py
@@ -2,6 +2,7 @@ import sys
 import numpy as np
 from PIL import Image, ImageDraw, ImageFont
 import math
+import random
 
 """
 Program format
@@ -13,3 +14,32 @@ mode: sequence or random (0 for sequence; 1 for random)
 
 """
 
+font = 'C:\\Windows\\Fonts\\micross.ttf'
+count = 0
+
+def argvinput():
+    arg = sys.argv
+    """if len(arg) != 6:
+        print('Wrong number of parameters')
+        sys.exit()"""
+    return arg
+
+def selectchar(charset, mode):
+    global count
+    if len(charset) == 1:
+        return charset
+    else:
+        if mode == 1:
+            return random.choice(charset)
+        else:
+            count += 1
+            return charset[(count - 1)%len(charset)]
+
+
+def main():
+    arg = argvinput()
+    img = Image.open(arg[1])
+    print(img)
+
+if __name__ == '__main__':
+    main()
